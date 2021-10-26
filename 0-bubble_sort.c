e2f9eb4e4d295315fb06d257f9a7286fc548a4f1
Akinyosoye Kayode <akinyosoyekaycee@gmail.com>
2021-10-26 15:34:29 -0700

    fixed errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,34 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array using the bubble sort algorithm.
- * @array: The array to sort.
- * @size: The length of the array.
+ * bubble_sort - sorts an array using the bubble algorithm
+ * @array: an array
+ * @size: size of the array 'number of elements in it'
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int tmp;
+	size_t i, n;
+	int big_val, flag;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		flag = 0;
+		for (n = 0; n < size - 1; n++)
 		{
-			if (array[j + 1] < array[j])
+			if (array[n] > array[n + 1])
 			{
-				SWAP(array, j, j + 1);
+				big_val = array[n];
+				array[n] = array[n + 1];
+				array[n + 1] = big_val;
+				flag = 1;
 				print_array(array, size);
 			}
 		}
+		if (flag == 0)
+			return;
 	}
+
 }
